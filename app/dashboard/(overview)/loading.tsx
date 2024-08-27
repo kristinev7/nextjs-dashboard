6a702af886da2d39aa789ab0c03138361884dae6
Kristinev7 <kveneles@gmail.com>
2024-08-26 21:34:21 -0400

    overview file

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,7 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+ 
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
+
+//   loading.tsx is a special Next.js file built on top of Suspense, it allows you to create fallback UI to show as a replacement while page content loads.
